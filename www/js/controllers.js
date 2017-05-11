f02aabb93e11e056a8f035d0a7b6d9ca85a5df95
acika00mk <acikagolemfraer1>
2017-05-12 01:50:37 +0200

    app functional

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,56 +1,80 @@
 angular.module('starter.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  // Form data for the login modal
-  $scope.loginData = {};
-
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
-
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
 })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
+
+.controller('HomeController', function($scope, $stateParams) {
+
+})
+.controller('MapController', function($scope, $stateParams) {
+navigator.geolocation.getCurrentPosition(showPosition);
+
+    function showPosition(position) {
+        var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        var mapOptions = {
+            center: latLng
+            , zoom: 15
+            , mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        $scope.map = new google.maps.Map(document.getElementById("mapBIG"), mapOptions);
+        google.maps.event.addListenerOnce($scope.map, 'idle', function () {
+            var marker = new google.maps.Marker({
+                map: $scope.map
+                , animation: google.maps.Animation.DROP
+                , position: latLng
+            });
+            var infoWindow = new google.maps.InfoWindow({
+                content: "Here I am!"
+            });
+//            google.maps.event.addListener(marker, 'click', function () {
+//                infoWindow.open($scope.map, marker);
+//            });
+//            google.maps.event.addListener($scope.map, 'click', function (event) {
+//                var marker = new google.maps.Marker({
+//                    position: event.latLng,
+//                    map: $scope.map
+//                });
+//            });
+        });
+    }
 })
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
+.controller('TipsController', function($scope, $stateParams) {
+
+})
+
+.controller('BrowseController', function($scope, $stateParams) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+
+    function showPosition(position) {
+        var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        var mapOptions = {
+            center: latLng
+            , zoom: 15
+            , mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+        google.maps.event.addListenerOnce($scope.map, 'idle', function () {
+            var marker = new google.maps.Marker({
+                map: $scope.map
+                , animation: google.maps.Animation.DROP
+                , position: latLng
+            });
+            var infoWindow = new google.maps.InfoWindow({
+                content: "Here I am!"
+            });
+//            google.maps.event.addListener(marker, 'click', function () {
+//                infoWindow.open($scope.map, marker);
+//            });
+//            google.maps.event.addListener($scope.map, 'click', function (event) {
+//                var marker = new google.maps.Marker({
+//                    position: event.latLng,
+//                    map: $scope.map
+//                });
+//            });
+        });
+    }
+
+
 });
